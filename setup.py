961dd118b2450250df6763f4a57dbec4d74bcb58
youngtaaal <63550727+youngtaaal@users.noreply.github.com>
2020-06-24 20:36:40 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+
+     name='flags',  
+
+     py_modules=["flags"],
+     
+     version='0.1',
+
+     author="Tal Klaiderman",
+
+     description="A Python wrapper for the Discord Python.",
+
+     classifiers=[
+
+         "Programming Language :: Python :: 3",
+
+         "License :: OSI Approved :: MIT License",
+
+         "Operating System :: OS Independent",
+
+     ],
+
+ )
